9b8a4d9cfb0181f95f31199660edae6680c7f966
manasal397 <59077105+manasal397@users.noreply.github.com>
2020-01-06 13:09:48 +0530

    Delete router.controller.js

--- router/router.controller.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const orderRoute = require('./order.router');
-const productRoute = require('./product.router');
-app.use('/', productRoute);
-app.use('/order', orderRoute);
-
-module.exports = app;
